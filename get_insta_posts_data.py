8e530abc34909d27fd4583e64fe93fe675532b45
mickaelyoshua7674 <mickaelyoshua@gmail.com>
2023-05-26 11:41:41 -0300

    add track of execution time for TCE, PREFJAMPA and TOTAL

--- get_insta_posts_data.py
+++ get_insta_posts_data.py
@@ -10,7 +10,7 @@ USERNAME_TCEPB = "tcepb"
 with open(f"{USERNAME_TCEPB}_posts_link.json", "r") as f:
     links = json.load(f)
 
-start = time.time()
+start_tce = time.time()
 bot_tcepb = InstaBot(USERNAME_TCEPB, CHROMEDRIVER_PATH, SECRET_INSTA)
 bot_tcepb.init_chromedriver(headless=True)
 bot_tcepb.login()
@@ -36,7 +36,7 @@ bot_tcepb.driver_quit()
 with open(f"{USERNAME_TCEPB}_posts_data.json", "w") as f:
     json.dump(posts, f)
 
-print(f"Execution time: {time.time()-start}s / {(time.time()-start)/60}min / {((time.time()-start)/60)/60}hrs")
+end_tce = time.time()
 
 
 USERNAME_PREF_JAMPA = "prefjoaopessoa"
@@ -44,7 +44,7 @@ USERNAME_PREF_JAMPA = "prefjoaopessoa"
 with open(f"{USERNAME_PREF_JAMPA}_posts_link.json", "r") as f:
     links = json.load(f)
 
-start = time.time()
+start_prefjampa = time.time()
 bot_pref_jampa = InstaBot(USERNAME_PREF_JAMPA, CHROMEDRIVER_PATH, SECRET_INSTA)
 bot_pref_jampa.init_chromedriver(headless=True)
 bot_pref_jampa.login()
@@ -70,4 +70,8 @@ bot_pref_jampa.driver_quit()
 with open(f"{USERNAME_PREF_JAMPA}_posts_data.json", "w") as f:
     json.dump(posts, f)
 
-print(f"Execution time: {time.time()-start}s / {(time.time()-start)/60}min / {((time.time()-start)/60)/60}hrs")
+end_prefjampa = time.time()
+
+print(f"Execution time TCE: {end_tce-start_tce}s / {(end_tce-start_tce)/60}min / {((end_tce-start_tce)/60)/60}hrs")
+print(f"Execution time PREFJAMPA: {end_prefjampa-start_prefjampa}s / {(end_prefjampa-start_prefjampa)/60}min / {((end_prefjampa-start_prefjampa)/60)/60}hrs")
+print(f"Execution time TOTAL: {end_prefjampa-start_tce}s / {(end_prefjampa-start_tce)/60}min / {((end_prefjampa-start_tce)/60)/60}hrs")
