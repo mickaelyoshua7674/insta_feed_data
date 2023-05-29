0d691fb4d8763cb41e869221d03ea8ded7d6687a
mickaelyoshua7674 <mickaelyoshua@gmail.com>
2023-05-29 09:54:30 -0300

    keep saving json file while collect data

---
+++ get_insta_posts_data_prefjampa.py
@@ -0,0 +1,63 @@
+from instaBot import InstaBot
+import json
+import time
+import os
+
+SECRET_INSTA = "mickaelyoshua_insta"
+CHROMEDRIVER_PATH = "chromedriver.exe"
+USERNAME = "prefjoaopessoa"
+
+start = time.time()
+# GETTING LINKS OF POSTS
+print("Getting posts links...")
+with open(f"{USERNAME}_posts_link.json", "r") as f:
+    links = json.load(f)
+print("Posts links collected.\n")
+
+# INITIALIZING INSTABOT
+start = time.time()
+bot = InstaBot(USERNAME, CHROMEDRIVER_PATH, SECRET_INSTA)
+bot.init_chromedriver(headless=True)
+bot.login()
+
+# GET LINKS WITH DATA ALREADY COLLECTED
+print("Getting link with data collected...")
+if os.path.exists(f"{USERNAME}_posts_data.json"): # if there is data collected
+    with open(f"{USERNAME}_posts_data.json", "r") as f:
+        data = json.load(f)
+    collected_links = [l["link"] for l in data]
+    print("Links collected.\n")
+else: # if there is no data collected
+    with open(f"{USERNAME}_posts_data.json", "w") as f:
+        json.dump([], f)
+    collected_links = []
+    print("There is no data collected.\n")
+
+# GET POSTS DATA
+for l in links:
+    if l not in collected_links: # if it wasn't collected
+        print(f"Collecting data from {l} ...")
+        bot.go_to_link(l)
+        time.sleep(3)
+        description = bot.get_post_description()
+        comments = bot.get_post_comments()
+        likes = bot.get_post_likes()
+
+        body = {
+            "link": l,
+            "description": description,
+            "comments": comments,
+            "likes": likes
+        }
+        print(f"Data collected from {l}")
+        print("Saving data...")
+        with open(f"{USERNAME}_posts_data.json", "r") as f:
+            data = json.load(f)
+        with open(f"{USERNAME}_posts_data.json", "w") as f: # append data and save
+            data.append(body)
+            json.dump(data, f)
+        print("Data saved.\n")
+
+bot.driver_quit()
+
+print(f"Execution time: {time.time()-start}s / {(time.time()-start)/60}min / {((time.time()-start)/60)/60}hrs")
--- test.py
+++ test.py
@@ -1,98 +1,15 @@
-from instaBot import *
+import json
 
-SECRET_INSTA = "mickaelyoshua_insta"
-TARGET_USERNAME = "tcepb"
-CHROMEDRIVER_PATH = "chromedriver.exe"
+body = {
+        "link": "instagram.com",
+        "description": "comemoração",
+        "comments": ["eba", "opa"],
+        "likes": "36 curtidas"
+    }
 
-PUBLISHMENT_CLASS = "._aabd._aa8k._al3l"
-PUBLISHMENT_LINK_CLASS = ".x1i10hfl.xjbqb8w.x6umtig.x1b1mbwd.xaqea5y.xav7gou.x9f619.x1ypdohk" + \
-                        ".xt0psk2.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5" + \
-                        ".x18d9i69.xkhd6sd.x16tdsg8.x1hl2dhg.xggy1nq.x1a2a7pz._a6hd"
-IMAGE_CLASS = ".x5yr21d.xu96u03.x10l6tqk.x13vifvy.x87ps6o.xh8yej3"
+with open("test.json", "r") as f:
+    data = json.load(f)
 
-LIKES_CLASS = ".x193iq5w.xeuugli.x1fj9vlw.x13faqbe.x1vvkbs.xt0psk2.x1i0vuye.xvs91rp.x1s688f.x5n08af.x10wh9bi.x1wdrske.x8viiok.x18hxmgj"
-VIEWS_CLASS = ".x1lliihq.x1plvlek.xryxfnj.x1n2onr6.x193iq5w.xeuugli.x1fj9vlw" + \
-                ".x13faqbe.x1vvkbs.x1s928wv.xhkezso.x1gmr53x.x1cpjm7i.x1fgarty" + \
-                ".x1943h6x.x1i0vuye.xvs91rp.x1s688f.x5n08af.x10wh9bi.x1wdrske.x8viiok.x18hxmgj"
-LIKED_BY_CLASS = ".x1lliihq.x1plvlek.xryxfnj.x1n2onr6.x193iq5w.xeuugli.x1fj9vlw" + \
-                    ".x13faqbe.x1vvkbs.x1s928wv.xhkezso.x1gmr53x.x1cpjm7i.x1fgarty" + \
-                    ".x1943h6x.x1i0vuye.xvs91rp.xo1l8bm.x5n08af.x10wh9bi.x1wdrske.x8viiok.x18hxmgj"
-
-PEOPLE_LIKED_CLASS = ".x1i10hfl.x1qjc9v5.xjbqb8w.xjqpnuy.xa49m3k.xqeqjp1.x2hbi6w.x13fuv20" + \
-                    ".xu3j5b3.x1q0q8m5.x26u7qi.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619" + \
-                    ".x1ypdohk.xdl72j9.x2lah0s.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r" + \
-                    ".x2lwn1j.xeuugli.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x1n2onr6.x16tdsg8" + \
-                    ".x1hl2dhg.xggy1nq.x1ja2u2z.x1t137rt.x1q0g3np.x87ps6o.x1lku1pv.x1a2a7pz" + \
-                    ".xh8yej3.x193iq5w.x1lliihq.x1dm5mii.x16mil14.xiojian.x1yutycm"
-OTHER_PEOPLE_CLASS = ".x1i10hfl.xjbqb8w.x6umtig.x1b1mbwd.xaqea5y.xav7gou.x9f619.x1ypdohk" + \
-                    ".xt0psk2.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5" + \
-                    ".x18d9i69.xkhd6sd.x16tdsg8.x1hl2dhg.xggy1nq.x1a2a7pz._a6hd"
-
-DESCRIPTION_CLASS = "._a9zs"
-COMMENTS_CLASS = "._a9ym"
-COMMENT_CLASS = "._aacl._aaco._aacu._aacx._aad7._aade"
-
-def print_error():
-    """Print the error message and exit script."""
-    traceback.print_exc()
-    print("Closing...")
-    sys.exit()
-
-driver = init_chromedriver(CHROMEDRIVER_PATH, headless=False)
-
-login(driver, "mickaelyoshua", "***")
-
-time.sleep(3)
-
-links = ["https://www.instagram.com/p/Cr1aiWnpSSG/", "https://www.instagram.com/p/CsCNwOFppsb/", "https://www.instagram.com/reel/BCEawKys_rF/"]
-
-likes = []
-for l in links:
-    driver.get(l)
-    time.sleep(3)
-    try:
-        t = driver.find_element(By.CSS_SELECTOR, LIKES_CLASS).text
-        print(f"t - {t}")
-        if re.search("like", t) or re.search("curti", t):
-            likes = t
-            print(f"Likes - {likes}")
-        else:
-            liked_by_link = [i.get_attribute("href") for i in driver.find_elements(By.CSS_SELECTOR, OTHER_PEOPLE_CLASS) if re.search("liked_by", i.get_attribute("href"))][0]
-            driver.get(liked_by_link)
-            time.sleep(3)
-            people = []
-            people_liked_obj = driver.find_elements(By.CSS_SELECTOR, PEOPLE_LIKED_CLASS)
-            for p in people_liked_obj:
-                people.append(p)
-            driver.find_element(By.CSS_SELECTOR, "body").send_keys(Keys.END)
-            time.sleep(2)
-
-            count = 0
-            while count < 10:
-                new_people_liked_obj = driver.find_elements(By.CSS_SELECTOR, PEOPLE_LIKED_CLASS)
-                for p in new_people_liked_obj:
-                    if p not in people_liked_obj:
-                        people.append(p)
-                driver.find_element(By.CSS_SELECTOR, "body").send_keys(Keys.END)
-                time.sleep(2)
-
-                if people_liked_obj == new_people_liked_obj:
-                    count += 1
-                
-                people_liked_obj = new_people_liked_obj
-            
-            likes = f"{len(people)} curtidas"
-            print(f"People likes - {likes}")
-
-    except NoSuchElementException:
-        driver.find_elements(By.CSS_SELECTOR, VIEWS_CLASS)[-1].click()
-        time.sleep(1)
-        likes = driver.find_element(By.CSS_SELECTOR, "._aauu").text
-        print(f"Views Likes - {likes}")
-        
-    except:
-        print("Error getting likes / Views / Liked By.")
-        print_error()
-
-# print(len(likes))
-# print(likes)
+with open("test.json", "w") as f:
+    data.append(body)
+    json.dump(data, f)
