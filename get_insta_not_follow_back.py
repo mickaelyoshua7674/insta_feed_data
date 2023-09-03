2cf756b633e3c6316db782fe07f362dcb0a14340
mickaelyoshua7674 <mickaelyoshua@gmail.com>
2023-09-03 16:55:47 -0300

    change to linux / update follow

--- get_insta_not_follow_back.py
+++ get_insta_not_follow_back.py
@@ -1,7 +1,7 @@
 from instaBot import InstaBot
 import time, json
 
-CHROMEDRIVER_PATH = "chromedriver.exe"
+CHROMEDRIVER_PATH = "/usr/lib/chromium-browser/chromedriver"
 USERNAME = "mickaelyoshua"
 
 # INITIALIZING INSTABOT
@@ -14,15 +14,15 @@ bot.login(login, password)
 
 bot.go_to_link(f"https://www.instagram.com/{USERNAME}/followers/")
 followers = bot.get_follow()
-with open("data/followers.json", "w") as f:
+with open("data_mickaelyoshua/followers.json", "w") as f:
     json.dump(followers, f)
 
 bot.go_to_link(f"https://www.instagram.com/{USERNAME}/following/")
 following = bot.get_follow()
-with open("data/following.json", "w") as f:
+with open("data_mickaelyoshua/following.json", "w") as f:
     json.dump(following, f)
 
-with open("data/not_follow_back.json", "w") as f:
+with open("data_mickaelyoshua/not_follow_back.json", "w") as f:
     json.dump(["https://www.instagram.com/"+f for f in following if f not in followers], f)
 
 print(f"Execution time: {time.time()-start}s / {(time.time()-start)/60}min / {((time.time()-start)/60)/60}hrs")
--- get_unfollowers.py
+++ get_unfollowers.py
@@ -1,7 +1,7 @@
 from instaBot import InstaBot
 import time, json
 
-CHROMEDRIVER_PATH = "chromedriver.exe"
+CHROMEDRIVER_PATH = "/usr/lib/chromium-browser/chromedriver"
 USERNAME = "mickaelyoshua"
 
 # INITIALIZING INSTABOT
@@ -17,14 +17,14 @@ new_followers = bot.get_follow()
 bot.go_to_link(f"https://www.instagram.com/{USERNAME}/following/")
 new_following = bot.get_follow()
 
-with open("data/followers.json", "r") as f:
+with open("data_mickaelyoshua/followers.json", "r") as f:
     loaded_followers = json.load(f)
-with open("data/unfollowers.json", "w") as f:
+with open("data_mickaelyoshua/unfollowers.json", "w") as f:
     json.dump(["https://www.instagram.com/"+f for f in new_followers if f not in loaded_followers], f)
 
-with open("data/followers.json", "w") as f:
+with open("data_mickaelyoshua/followers.json", "w") as f:
     json.dump(new_followers, f)
-with open("data/following.json", "w") as f:
+with open("data_mickaelyoshua/following.json", "w") as f:
     json.dump(new_following, f)
 
 print(f"Execution time: {time.time()-start}s / {(time.time()-start)/60}min / {((time.time()-start)/60)/60}hrs")
